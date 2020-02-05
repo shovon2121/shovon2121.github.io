8d2f99fbdb76af609619e6efd72b2ee2755c77b2
shovon2121 <shovon15-7671@diu.edu.bd>
2020-02-06 05:13:40 +0600

    github site

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(window).on('load', function () {
+	$('#status').fadeOut();
+	$('#preloader').delay(355).fadeOut();
+});
+
+
+
+
+
+var typed = new Typed('#typed', {
+	stringsElement: '#typed-strings',
+	typeSpeed: 35,
+	backSpeed: 35,
+	startDelay: 500,
+	loop: true,
+	loopCount: Infinity
+});
+
+
+
+/*-----------------------------------------------------Counter-----------------------------------------*/
+
+$(function() {
+	new WOW().init();
+})
+// name
+$(window).on('load', function() {
+	$("#fname").addClass("animated bounceInDown infinite");
+	$("#arrow-down i").addClass("animated fadeInDown infinite");
+});
+
+
+/*---------------porjects-------------------------------------------------- */
+$(function() {
+	$("#projects").owlCarousel({
+		items: 0,
+		autoplay: true,
+		smartSpeed: 700,//time to slide
+		loop: true,
+		autoplayHoverPause: true,
+		dots: false,
+		nav: true
+
+	});
+});
